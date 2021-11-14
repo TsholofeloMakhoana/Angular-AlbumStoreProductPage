34dc943f3a29959b0826f8628c4ec4144886fa52
tsholofelomakhoana <tsholofelo.makhoana@gmail.com>
2021-11-14 11:40:40 +0200

    solution 3

--- src/app/album.ts
+++ src/app/album.ts
@@ -1,8 +1,9 @@
 import { Track } from './track';
 
+
 export interface Album {
-  name;
-  releaseDate;
-  coverImage;
-  tracks;
+  name: string;
+  releaseDate: string;
+  coverImage: string;
+  tracks: Track[];
 }
